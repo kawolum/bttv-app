a4d692d2e0b804f24aec92eae856587cd4c823ea
Ka Lum <kalum@Kas-Mac.local>
2017-01-03 21:59:58 -0600

    hi

---
+++ BTTVCHAT/MessageTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MessageTableViewCell.swift
+//  BTTVCHAT
+//
+//  Created by Ka Lum on 1/3/17.
+//  Copyright © 2017 Ka Lum. All rights reserved.
+//
+
+import UIKit
+
+class MessageTableViewCell: UITableViewCell {
+    @IBOutlet weak var messageLabel: UILabel!
+}
